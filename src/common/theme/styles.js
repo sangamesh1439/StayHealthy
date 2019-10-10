5ca4ab6af49600bf4d6f4ac58671d3ae9a37f9e5
sangameshrently <sangamesh.r@rently.com>
2019-10-10 09:03:45 -0700

    add dummy component for Recipies and Recipe

---
+++ src/common/theme/styles.js
@@ -0,0 +1,9 @@
+import { ScaledSheet } from 'react-native-size-matters';
+
+export const styles = ScaledSheet.create({
+    headerTextStyle: {
+        fontSize:'40@ms',
+        color:"white",
+        padding:10
+    },
+});
